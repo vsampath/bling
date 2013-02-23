216d6427b119b49fa0e21f7000f4eb820513256c
Varun Sampath <vsampath@ieee.org>
2013-02-23 12:11:19 -0800

    we got purple!

--- bling.ino
+++ bling.ino
@@ -1,14 +1,28 @@
+#include <SPI.h>
+
 #include <Adafruit_WS2801.h>
 
-#define CPIN 52
-#define DPIN 53
+#define CPIN ( (uint8_t)52 )
+#define DPIN ( (uint8_t)53 )
 
 // construct LED driver object
-Adafruit_WS2801( 1,DPIN, CPIN, WS2801_RGB );
+Adafruit_WS2801 LedDriver ( 
+  (uint16_t) 1,
+  DPIN, 
+  CPIN, 
+  (uint8_t)WS2801_RGB 
+ );
 
 void setup() {
   // put your setup code here, to run once:
-  
+  LedDriver.begin();
+  LedDriver.setPixelColor( 
+    (uint16_t) 0,
+    (uint8_t) 0xFF,
+    (uint8_t) 0,
+    (uint8_t) 0xFF
+    );
+  LedDriver.show();
 }
 
 void loop() {
