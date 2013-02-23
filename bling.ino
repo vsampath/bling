e1391799c6c7691f09876cc52c6d142c2e7ae7e9
Varun Sampath <vsampath@ieee.org>
2013-02-23 11:55:49 -0800

    adding skeleton arduino file

---
+++ bling.ino
@@ -0,0 +1,17 @@
+#include <Adafruit_WS2801.h>
+
+#define CPIN 52
+#define DPIN 53
+
+// construct LED driver object
+Adafruit_WS2801( 1,DPIN, CPIN, WS2801_RGB );
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
